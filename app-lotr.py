bf891fea8ed577872f72f5caa6c93f844e6794a5
derickson <david.erickson@gmail.com>
2023-04-14 22:17:40 -0400

    remove speak and alternate chunk

--- app-lotr.py
+++ app-lotr.py
@@ -4,13 +4,12 @@ import lib_llm
 import lib_embeddings
 import lib_vectordb
 
-import lib_speak
 
 
 
 config = {
     "bookName" : "The Lord of The Rings",
-    "bookIndexName": "book_lotr_embeddings",
+    "bookIndexName": "book_lotr_embeddings-big",
     "bookFilePath": "./data/lotr-utf8.txt"
 }
 
@@ -28,16 +27,16 @@ db, url = lib_vectordb.setup_vectordb(hf,index_name)
 llm_chain_informed= lib_llm.make_the_llm()
 
 ## Load the book
-lib_book_parse.loadBook(bookFilePath,url,hf,db,index_name)
+lib_book_parse.loadBookBig(bookFilePath,url,hf,db,index_name)
 
 ## how to ask a question
 def ask_a_question(question):
-    print("The Question at hand: "+question)
+    # print("The Question at hand: "+question)
 
     ## 3. get the relevant chunk from Elasticsearch for a question
-    print(">> 3. get the relevant chunk from Elasticsearch for a question")
+    # print(">> 3. get the relevant chunk from Elasticsearch for a question")
     similar_docs = db.similarity_search(question)
-    print(similar_docs[0].page_content)
+    print(f'The most relevant passage: \n\t{similar_docs[0].page_content}')
 
     ## 4. Ask Local LLM context informed prompt
     # print(">> 4. Asking The Book ... and its response is: ")
@@ -48,10 +47,10 @@ def ask_a_question(question):
 
 # The conversational loop
 
-lib_speak.speak(f'I am the book, "{bookName}", ask me any question: ')
+print(f'I am the book, "{bookName}", ask me any question: ')
 
 while True:
     command = input("User Question>> ")
     response = ask_a_question(command)
-    lib_speak.speak(f"\n\n I think the answer is : {response}\n")
+    print(f"\n\n I think the answer is : {response}\n")
 
--- lib_book_parse.py
+++ lib_book_parse.py
@@ -4,6 +4,21 @@ from langchain.document_loaders import TextLoader
 
 from elasticsearch import Elasticsearch
 
+def parse_book(filepath):
+    loader = TextLoader(filepath)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=0)
+    docs = text_splitter.split_documents(documents)
+    return docs
+
+def parse_triplets(filepath):
+    docs = parse_book(filepath)
+    result = []
+    for i in range(len(docs) - 2):
+        concat_str = docs[i].page_content + " " + docs[i+1].page_content + " " + docs[i+2].page_content
+        result.append(concat_str)
+    return result
+    #db.from_texts(docs, embedding=hf, elasticsearch_url=url, index_name=index_name)
 
 ## load book utility
 ## params
@@ -14,23 +29,35 @@ from elasticsearch import Elasticsearch
 ##  index_name: name of index to use in ES
 ##
 ##  will check if the index_name exists already in ES url before attempting split and load
-def loadBook(filepath, url, hf, db, index_name):
+def loadBookTriplets(filepath, url, hf, db, index_name):
 
     with Elasticsearch([url], verify_certs=True) as es:
         ## Parse the book if necessary
         if not es.indices.exists(index=index_name):
             print(f'\tThe index: {index_name} does not exist')
             print(">> 1. Chunk up the Source document")
-            loader = TextLoader(filepath)
-            documents = loader.load()
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=20)
-            docs = text_splitter.split_documents(documents)
-            # for doc in docs:
-            #     content = doc.page_content
-            #     mylen = len(content)
-            #     print(mylen)
-            #     print(content)
+            
+            results = parse_triplets(filepath)
+
             print(">> 2. Index the chunks into Elasticsearch")
-            db.from_documents(docs, embedding=hf, elasticsearch_url=url, index_name=index_name)
+            
+            db.from_texts(results, embedding=hf, elasticsearch_url=url, index_name=index_name)
         else:
             print("\tLooks like the book is already loaded, let's move on")
+
+def loadBookBig(filepath, url, hf, db, index_name):
+
+    with Elasticsearch([url], verify_certs=True) as es:
+        ## Parse the book if necessary
+        if not es.indices.exists(index=index_name):
+            print(f'\tThe index: {index_name} does not exist')
+            print(">> 1. Chunk up the Source document")
+            
+            docs = parse_book(filepath)
+
+            print(">> 2. Index the chunks into Elasticsearch")
+            
+            db.from_texts(docs, embedding=hf, elasticsearch_url=url, index_name=index_name)
+        else:
+            print("\tLooks like the book is already loaded, let's move on")
+
